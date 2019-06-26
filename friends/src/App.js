cfc3c4f9076c413dc56a134fc9ae49152a24d1d2
cwern40 <chris.wernli@hotmail.com>
2019-06-25 18:45:20 -0600

    I imported the information from the other host server using ajax and I created the component Friendlist

---
+++ friends/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import axios from 'axios';
+import FriendContainer from './components/FriendsContainer'
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/friends')
+      .then(response => {
+        this.setState({
+          friends: response.data
+        })
+      })
+      .catch(err => {
+        console.log("Error:", err);
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <FriendContainer friends={this.state.friends} />
+      </div>
+    );
+  }
+}
+export default App;
